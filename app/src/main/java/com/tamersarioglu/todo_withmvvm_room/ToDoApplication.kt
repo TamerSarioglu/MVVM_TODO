bb9ac4d7902b6d047d4f3a6e0161782411cd858a
Tamer Sarıoğlu <tamerthedark@hotmail.com>
2021-01-06 23:14:25 +0300

    New Repo Of MVVM from Coding in Flow for Education

---
+++ app/src/main/java/com/tamersarioglu/todo_withmvvm_room/ToDoApplication.kt
@@ -0,0 +1,8 @@
+package com.tamersarioglu.todo_withmvvm_room
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ToDoApplication: Application() {
+}
---
+++ app/src/main/java/com/tamersarioglu/todo_withmvvm_room/util/Utils.kt
@@ -0,0 +1,4 @@
+package com.tamersarioglu.todo_withmvvm_room.util
+
+val <T> T.exhaustive: T
+    get() = this
