8a41ea01896706c8780163b10118b1c494295670
Tamer Sarıoğlu <tamerthedark@hotmail.com>
2021-01-24 01:41:16 +0300

    Kod Bloğu 1

--- app/src/main/java/com/tamersarioglu/todo_withmvvm_room/data/Tasks.kt
+++ app/src/main/java/com/tamersarioglu/todo_withmvvm_room/data/Tasks.kt
@@ -8,13 +8,13 @@ import java.text.DateFormat
 
 @Entity(tableName = "task_table")
 @Parcelize
-data class Task(
-    val name:String,
+data class Tasks(
+    val name: String,
     val important: Boolean = false,
     val completed: Boolean = false,
     val created: Long = System.currentTimeMillis(),
     @PrimaryKey(autoGenerate = true) val id: Int = 0
-): Parcelable {
-    val calculatedDateFormat: String
-        get() = DateFormat.getDateTimeInstance().format(created)
+) : Parcelable {
+    val createdDateFormatted: String
+        get() = DateFormat.getDateTimeInstance().format(completed)
 }
