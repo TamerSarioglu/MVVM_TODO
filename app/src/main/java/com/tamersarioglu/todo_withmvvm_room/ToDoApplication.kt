a69cb0e2241927f536d9939813051503bc468c44
Tamer Sarıoğlu <tamerthedark@hotmail.com>
2021-01-24 01:59:14 +0300

    Kod Bloğu 1

--- app/src/main/java/com/tamersarioglu/todo_withmvvm_room/ToDoApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.tamersarioglu.todo_withmvvm_room
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class ToDoApplication: Application() {
-}
--- app/src/main/java/com/tamersarioglu/todo_withmvvm_room/util/Utils.kt
+++
@@ -1,4 +0,0 @@
-package com.tamersarioglu.todo_withmvvm_room.util
-
-val <T> T.exhaustive: T
-    get() = this
